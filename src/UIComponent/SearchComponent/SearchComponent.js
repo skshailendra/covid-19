43d3161f3b75099982935b789b1a80d15eb72114
skshailendra <singh.shailendra.ind2@gmail.com>
2020-05-29 01:29:42 +0530

    Search functionality

--- src/UIComponent/SearchComponent/SearchComponent.js
+++ src/UIComponent/SearchComponent/SearchComponent.js
@@ -1,5 +1,6 @@
-import React ,{ useState , useRef, useReducer}from 'react';
+import React ,{ useState , useRef, useContext, useEffect}from 'react';
 
+import {FetchDataContext} from '../../context/fetch-data';
 import { FontAwesomeIcon, } from '@fortawesome/react-fontawesome';
 import { faSearch,faTimesCircle } from "@fortawesome/free-solid-svg-icons";
 import './SearchComponent.scss';
@@ -7,6 +8,11 @@ import './SearchComponent.scss';
 const SearchComponent = props =>{
     const [searchFocus, setSearchFocus] = useState(false);
     const [searchValue, setSearchValue] = useState('');
+    const fetchCovidData = useContext(FetchDataContext);
+    const [stateData,setStateData] = useState([]);
+    const [stateDistrict,setStateDistrict] = useState([]);
+    const [searchList, setSearchList] = useState([]);
+    let tempSearchList = [], tempDistrictList= [];
     const searchRef= useRef();
     const onChangeHandler = (event) =>{
         setSearchValue(searchRef.current.value);
@@ -21,6 +27,47 @@ const SearchComponent = props =>{
         setSearchFocus(false);
         setSearchValue('');
     }
+
+    useEffect(()=>{
+        if(fetchCovidData){
+          setStateData(fetchCovidData.statewise.slice(1));
+          setStateDistrict(fetchCovidData.stateDistrict);
+        }
+    },[fetchCovidData]);
+
+    useEffect(() => {
+        console.log("search value..",searchRef.current.value);
+        
+        
+        const filterStateData = stateData.filter( (data)=>{
+            return data.state.toLowerCase().includes((searchRef.current.value.toLowerCase()))
+        });
+        //tempSearchList = [...searchList]; // Destructure array list
+        tempSearchList.push(...filterStateData);
+       //setSearchList(tempSearchList);
+
+        // Searching in District List
+        stateDistrict.forEach( (data)=>{
+            const filterDistrictList = data.districtData.filter((dist)=>{
+                return dist.district.toLowerCase().includes((searchRef.current.value.toLowerCase()))
+            });
+            if(filterDistrictList.length > 0){
+                filterDistrictList.map((filDist)=>{
+                    filDist.isDistrict = true;
+                    filDist.state = data.state;
+                    filDist.statecode = data.statecode;
+                })
+                console.log("-------",filterDistrictList)
+                tempDistrictList.push(...filterDistrictList);
+            }           
+        });
+        
+        setSearchList([...tempSearchList,...tempDistrictList]);
+       
+    },[searchValue]);
+    useEffect(()=>{
+        console.log(searchList);
+    },[searchList])
     return (
         <>
             <div className="search-wrap">
@@ -38,7 +85,52 @@ const SearchComponent = props =>{
                     {searchFocus &&
                     <div className="search-result">
                         <ul className="search-result__list">
-                            <li className="search-result__item">
+                            {
+                            searchList && 
+                            searchList.map((search,idx)=>(
+                                <li key={idx} className="search-result__item">
+                                    {search.isDistrict && 
+                                    <>
+                                    <div className="search-result__name">
+                                        <span className="search-result__district-name">
+                                        {search.district}
+                                        </span>
+                                        <span className="search-result__dist-state-name">
+                                            {search.state} 
+                                        </span>
+                                    </div>
+                                    <div className="search-result__count">
+                                        <span className="search-result__status-text">
+                                            Confirmed
+                                        </span>
+                                        <span className="search-result__status-count">
+                                            {search.confirmed}
+                                        </span>
+                                        
+                                    </div>
+                                    </>
+                                    }
+                                    {!search.isDistrict && 
+                                    <>
+                                    <div className="search-result__state-name">
+                                        {search.state}
+                                    </div>
+                                    <div className="search-result__count">
+                                        <span className="search-result__status-text">
+                                            Confirmed
+                                        </span>
+                                        <span className="search-result__status-count">
+                                            {search.confirmed}
+                                        </span>
+                                        
+                                    </div>
+                                    </>
+                                    }
+                                    
+                                </li>
+                            ))
+                            }
+                            {/* <li className="search-result__item">
                                 <div className="search-result__name">
                                     Maharastra
                                 </div>
@@ -65,7 +157,7 @@ const SearchComponent = props =>{
                                   <button className="btn-inline"> Show All <span>&rarr;</span></button>
                                 </figure>
 
-                            </li>
+                            </li> */}
                         </ul>
                     </div>
                     }
