2fe5a11f5510d93d9aa3856d67ad7ab258eaa98c
skshailendra <singh.shailendra.ind2@gmail.com>
2020-06-04 02:52:37 +0530

    Dropdown Px

--- src/UIComponent/Backdrop/Backdrop.js
+++ src/UIComponent/Backdrop/Backdrop.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Backdrop.scss';
 
 const Backdrop = React.memo(props =>(
-    <div className="backdrop" onClick={props.clickBackdrop}></div>
+    <div className={`backdrop ${props.transparent}`} onClick={props.clickBackdrop}></div>
 ));
 
 export default Backdrop;
