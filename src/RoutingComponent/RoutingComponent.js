036d4e4b4ec47c5b55a218390ae71b0c2644dbec
skshailendra <singh.shailendra.ind2@gmail.com>
2020-06-10 20:23:39 +0530

    Lazy loading Route

--- src/RoutingComponent/RoutingComponent.js
+++ src/RoutingComponent/RoutingComponent.js
@@ -1,6 +1,7 @@
 import React , {Suspense, lazy} from 'react';
 import {Route} from 'react-router-dom';
-const MainContent = lazy(()=> import('../MainContent/MainContent'));
+import MainContent from '../MainContent/MainContent';
+import Loading from '../UIComponent/Loading/Loading';
 const MainStateContent = lazy(()=> import ('../MainStateContent/MainStateContent'));
 const IndiaComponent = lazy(()=> import('../MapComponent/IndiaComponent'));
 
@@ -8,7 +9,8 @@ const IndiaComponent = lazy(()=> import('../MapComponent/IndiaComponent'));
 const RoutingComponent = props =>{
     return (
         <>            
-            <Suspense fallback={<div>Loading ...</div>}>
+            
+            <Suspense fallback={<Loading/>}>
                 <Route path="/" exact component ={MainContent} />
                 <Route path="/map" exact component ={IndiaComponent} />
                 <Route path="/state/:statecode" component ={MainStateContent} />
