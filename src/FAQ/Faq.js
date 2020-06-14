678caac082ea5991cd162000152bff2ed9892272
geet365 <sangeeta4521@gmail.com>
2020-06-15 00:34:14 +0530

    updating question

--- src/FAQ/Faq.js
+++ src/FAQ/Faq.js
@@ -8,14 +8,14 @@ const questionAnswerList = [
   {
     id: 1,
     qn: "What is a coronavirus?",
-    ans: `Coronaviruses are a large family of viruses which may cause illness in animals or humans.  In humans, several coronaviruses are known to cause respiratory infections ranging from the common cold to more severe diseases such as Middle East Respiratory Syndrome (MERS) and Severe Acute Respiratory Syndrome (SARS).
-         The most recently discovered coronavirus causes coronavirus disease COVID-19.`,
+    ans: `As explained by the WHO, the coronaviruses are a large family of viruses which may cause illness in animals or humans.  In humans, several coronaviruses are known to cause respiratory infections ranging from the common cold to more severe diseases such as Middle East Respiratory Syndrome (MERS) and Severe Acute Respiratory Syndrome (SARS).
+    The most recently discovered coronavirus causes coronavirus disease COVID-19.`,
   },
   {
     id: 2,
     qn: "What is COVID-19?",
-    ans: `COVID-19 is the infectious disease caused by the most recently discovered coronavirus. This new virus and disease were unknown before the outbreak began in Wuhan, China, in December 2019. 
-          COVID-19 is now a pandemic affecting many countries globally.`,
+    ans: `As explained by the WHO, COVID-19 is the infectious disease caused by the most recently discovered coronavirus. This new virus and disease were unknown before the outbreak began in Wuhan, China, in December 2019. 
+    COVID-19 is now a pandemic affecting many countries globally.`,
   },
   {
     id: 3,
