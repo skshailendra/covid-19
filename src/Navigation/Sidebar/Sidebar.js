8ad4df484e2491c1fe8c9cfc75035ac79e9294ea
geet365 <sangeeta4521@gmail.com>
2020-06-14 13:41:29 +0530

    WIP

--- src/Navigation/Sidebar/Sidebar.js
+++ src/Navigation/Sidebar/Sidebar.js
@@ -1,7 +1,7 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Sidebar.scss';
 import {NavLink} from 'react-router-dom';
-import { faHome, faMapMarkerAlt , faChartLine } from "@fortawesome/free-solid-svg-icons";
+import { faHome, faMapMarkerAlt , faChartLine, faQuestionCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon, } from '@fortawesome/react-fontawesome';
 
 const Sidebar = React.memo(props =>{
@@ -39,6 +39,15 @@ const Sidebar = React.memo(props =>{
                             <FontAwesomeIcon icon={faChartLine}  color="#f5a616" className="side-nav__icon"/>
                             <div className={`side-nav__text ${propsSidebarOpen ? "open":""}`}>
                                 <span>Analysis</span>
+                            </div>
+                        </NavLink> 
+                    </li>
+
+                    <li className="side-nav__items">
+                        <NavLink to='/faq' className={`side-nav__link ${propsSidebarOpen ? "open":""}`} activeClassName="side-nav__link--active">
+                            <FontAwesomeIcon icon={faQuestionCircle}  color="#f5a616" className="side-nav__icon"/>
+                            <div className={`side-nav__text ${propsSidebarOpen ? "open":""}`}>
+                                <span>FAQ</span>
                             </div>
                         </NavLink> 
                     </li>
--- src/RoutingComponent/RoutingComponent.js
+++ src/RoutingComponent/RoutingComponent.js
@@ -4,18 +4,19 @@ import MainContent from '../MainContent/MainContent';
 import Loading from '../UIComponent/Loading/Loading';
 const MainStateContent = lazy(()=> import ('../MainStateContent/MainStateContent'));
 const IndiaComponent = lazy(()=> import('../MapComponent/IndiaComponent'));
+const FaqComponent = lazy(() => import('../FAQ/Faq'));
 
 
 const RoutingComponent = props =>{
     return (
-        <>            
-            
+        <>                     
             <Suspense fallback={<div></div>}>
                 <Route path="/" exact component ={MainContent} />
                 <Route path="/map" exact component ={IndiaComponent} />
                 <Route path="/state/:statecode" component ={MainStateContent} />
+                <Route path="/faq" component={FaqComponent}/>
             </Suspense>
-        </>      
+        </>
     )
 };
 
