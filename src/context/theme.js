ba942a2ac19d074c76c031909e4430f0f1453b93
geet365 <sangeeta4521@gmail.com>
2020-06-16 12:11:48 +0530

    nightmode aumated with time

--- src/context/theme.js
+++ src/context/theme.js
@@ -1,28 +1,32 @@
 import React ,{useState} from 'react';
 
 export const ThemeContext = React.createContext({
-    nightMode: null,
-    thememode:'',
-    toggleTheme: () =>{}
+  nightMode: null,
+  thememode:'',
+  toggleTheme: () => {}
 });
 
-const ThemeModeProvider = props =>{
-    const [nightMode,setNightMode] = useState(false);
-    const [thememode,setThememode] = useState('daymode');
-    const toggleTheme = ()=>{
-        const theme = nightMode ? 'daymode': 'nightmode'; 
-        setNightMode(!nightMode);
-        setThememode(theme);
-    }
-    return (
-        <ThemeContext.Provider value={{
-            nightMode:nightMode,
-            thememode:thememode,
-            toggleTheme: toggleTheme
-        }}>
-{props.children}
-        </ThemeContext.Provider>
-    )
+const ThemeModeProvider = props => {
+  const initNightMode = localStorage.getItem('nightmode') === 'true';
+  const initThemeMode = initNightMode ? 'nightmode' : 'daymode';
+  const [nightMode,setNightMode] = useState(initNightMode);
+  const [thememode,setThememode] = useState(initThemeMode);
+
+  const toggleTheme = () => {
+    const theme = nightMode ? 'daymode': 'nightmode'; 
+    setNightMode(!nightMode);
+    setThememode(theme);
+  }
+
+  return (
+    <ThemeContext.Provider value = {{
+      nightMode:nightMode,
+      thememode:thememode,
+      toggleTheme: toggleTheme
+    }}>
+      {props.children}
+    </ThemeContext.Provider>
+  )
 };
 
 export default ThemeModeProvider;
