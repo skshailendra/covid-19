4b7a8b964a0fbf4989bccb9714157a9c06cb9738
skshailendra <singh.shailendra.ind2@gmail.com>
2020-06-17 04:05:32 +0530

    Adding Nightmode to Loading

--- src/UIComponent/Loading/Loading.js
+++ src/UIComponent/Loading/Loading.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React ,{useContext}from 'react';
 import { faCircleNotch} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon, } from '@fortawesome/react-fontawesome';
 import './Loading.scss';
+import {ThemeContext} from '../../context/theme';
 const Loading = props =>{
+    const {thememode} = useContext(ThemeContext);
     return (
-        <div className="loading">
+        <div className={`loading ${thememode}`}>
             <FontAwesomeIcon icon={faCircleNotch}  color="#c3c3c3"  size="3x"/>
         </div>
     )
