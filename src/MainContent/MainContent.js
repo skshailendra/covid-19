73bf08a8fc5226d3253b7b1b92c999207eba3d1f
skshailendra <singh.shailendra.ind2@gmail.com>
2020-06-16 20:14:37 +0530

    Change Table Component Loading Timing

--- src/MainContent/MainContent.js
+++ src/MainContent/MainContent.js
@@ -9,15 +9,11 @@ const TableComponent = lazy(()=>import('../UIComponent/TableComponent/TableCompo
 const MainContent = props =>{
     const [showTable,setShowTable] = useState(false);
     const {thememode} = useContext(ThemeContext);
+    
     useEffect(()=>{
-       const handleScrollEvent = () =>{
-        if ((window.innerHeight + window.pageYOffset + 200) >= document.body.offsetHeight) {
-           setShowTable(true);
-         }
-       };
-       // setShowTable(true);
-       window.addEventListener('scroll', handleScrollEvent);
-       return(()=> window.removeEventListener('scroll',handleScrollEvent));
+        setTimeout(()=>{
+            setShowTable(true);
+        },20);
     },[]);
     return (
         <>
