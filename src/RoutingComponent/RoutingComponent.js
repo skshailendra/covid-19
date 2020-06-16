6a71807ad9381f821c9de507ec8f92b70d744e88
skshailendra <singh.shailendra.ind2@gmail.com>
2020-06-17 04:06:16 +0530

    Adding Loading in Route

--- src/RoutingComponent/RoutingComponent.js
+++ src/RoutingComponent/RoutingComponent.js
@@ -3,13 +3,14 @@ import {Route} from 'react-router-dom';
 import MainContent from '../MainContent/MainContent';
 import FaqComponent from '../FAQ/Faq';
 import InfoComponent from '../Info/Info';
+import Loading from '../UIComponent/Loading/Loading';
 const MainStateContent = lazy(()=> import ('../MainStateContent/MainStateContent'));
 const IndiaComponent = lazy(()=> import('../MapComponent/IndiaComponent'));
 
 const RoutingComponent = props =>{
   return (
     <>                     
-      <Suspense fallback={<div></div>}>
+      <Suspense fallback={<div><Loading/></div>}>
         <Route path="/" exact component ={MainContent} />
         <Route path="/map" exact component ={IndiaComponent} />
         <Route path="/state/:statecode" component ={MainStateContent} />
