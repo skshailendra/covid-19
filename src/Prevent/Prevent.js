a8332e61333277bc57f4ab08148d4e457e53f964
geet365 <sangeeta4521@gmail.com>
2020-06-17 23:37:02 +0530

    symptoms UI added

--- src/Prevent/Prevent.js
+++ src/Prevent/Prevent.js
@@ -15,11 +15,30 @@ const Prevent = () => {
         <div className='prev-section'>
           <div class='box sec1'>
             <h3>SYMPTOMS</h3>
+            <p>Symptoms may varies person to person, here are some common.</p>
             <div className='img-section'>
               <span className='icon-sec'>
                 <FontAwesomeIcon icon={faHeadSideCough}  color="#a23d83" className="icon"/>
               </span>
               <p>DRY COUGH</p>
+            </div>
+            <div className='img-section'>
+              <span className='icon-sec'>
+                <FontAwesomeIcon icon={faThermometerFull}  color="#a23d83" className="icon"/>
+              </span>
+              <p>HIGH FEVER</p>
+            </div>
+            <div className='img-section'>
+              <span className='icon-sec'>
+                <FontAwesomeIcon icon={faHeadSideCoughSlash}  color="#a23d83" className="icon"/>
+              </span>
+              <p>SORE THROAT</p>
+            </div>
+            <div className='img-section'>
+              <span className='icon-sec'>
+                <FontAwesomeIcon icon={faLungsVirus}  color="#a23d83" className="icon"/>
+              </span>
+              <p>DIFFICULTY IN BREATHING</p>
             </div>
           </div>
           <div class='box sec2'>2
