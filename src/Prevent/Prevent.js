01f8f2fa99008307e6c7284556f27261dca14e1f
geet365 <sangeeta4521@gmail.com>
2020-06-17 23:16:22 +0530

    added images with card implementaion

--- src/Prevent/Prevent.js
+++ src/Prevent/Prevent.js
@@ -2,6 +2,9 @@ import React, { useContext } from "react";
 import { ThemeContext } from '../context/theme';
 import './Prevent.scss';
 import Overview from "../Overview/Overview";
+import { faHeadSideCough, faThermometerFull, faClinicMedical, 
+  faHeadSideCoughSlash, faHeadSideMask, faLungsVirus, } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon, } from '@fortawesome/react-fontawesome';
 
 const Prevent = () => {
   const { thememode } = useContext(ThemeContext);
@@ -11,7 +14,13 @@ const Prevent = () => {
       <div className='prev-content'>
         <div className='prev-section'>
           <div class='box sec1'>
-            1
+            <h3>SYMPTOMS</h3>
+            <div className='img-section'>
+              <span className='icon-sec'>
+                <FontAwesomeIcon icon={faHeadSideCough}  color="#a23d83" className="icon"/>
+              </span>
+              <p>DRY COUGH</p>
+            </div>
           </div>
           <div class='box sec2'>2
           </div>
