df44b91386dc678995f9a905f5614eff030779c6
geet365 <sangeeta4521@gmail.com>
2020-06-20 04:03:41 +0530

    Prevention section final

--- src/Prevent/PreventRow/MythRow.js
+++ src/Prevent/PreventRow/MythRow.js
@@ -9,7 +9,7 @@ const MythRow = (props) => {
   return (
     <>
       <div className='content-border'>
-        <div className='myth-header'><span>MYTH BUSTERS COVID-19</span></div>
+        <div className={`myth-header ${thememode}`}><span>MYTH BUSTERS COVID-19</span></div>
         <div className='sign'>       
           <FontAwesomeIcon className='icon-size' icon={faTimes} color='red'/>
           <FontAwesomeIcon className='icon-size' icon={faCheck} color='#26ce31'/>
