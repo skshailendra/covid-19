577b4da3c759579242d22290495351e3c185e8a7
skshailendra <singh.shailendra.ind2@gmail.com>
2020-06-21 22:51:38 +0530

    Adding Custom Tooltip in tiny bar

--- src/GraphComponent/TinyBarGraph/TinyBarGraph.js
+++ src/GraphComponent/TinyBarGraph/TinyBarGraph.js
@@ -5,11 +5,12 @@ import {BarChart, Bar, XAxis, Tooltip, LabelList}from 'recharts';
 
 import useDeviceAgent from '../../hooks/device-agent';
 import {ThemeContext} from '../../context/theme';
+
 const TinyBarGraph = props =>{
     const {device} = useDeviceAgent();
     const [chartWidth, setChartWidth] = useState(800);
     const [chartHeight, setChartHeight] = useState(400);
-    const {nightMode} = useContext(ThemeContext);
+    const {nightMode,thememode} = useContext(ThemeContext);
     const {filterCaseType} = props;
     useEffect(()=>{
       if(device && device.isExtraLargeDevice){
@@ -41,9 +42,27 @@ const TinyBarGraph = props =>{
         
          return (
           <g transform={`translate(${x},${y})`}>
-            <text x={0} y={0} dy={16} className="xaxis-label" textAnchor="middle" fill={nightMode? "#fff":"#666"} transform="rotate(0)">{`${payload.value.slice(0,7)}${payload.value.length > 7 ?'...':''}`}</text>
+            <text x={0} y={0} dy={16} className={`xaxis-label ${thememode}`} textAnchor="middle" fill={nightMode? "#fff":"#666"} transform="rotate(0)">{`${payload.value.slice(0,7)}${payload.value.length > 7 ?'...':''}`}</text>
           </g>
         );
+    }
+    const tooltipContent = (tooltipProps) => {
+     
+      let payload;
+      if(tooltipProps.payload && tooltipProps.payload.length>0){
+        debugger;
+        payload = tooltipProps.payload[0];
+      }
+      return <div className={`custom-tooltip ${thememode}`}>
+        { payload && <>
+          <h4 className="tooltip-heading">{payload.payload.district}</h4>
+          <span  className="confirmed">Confirmed: {payload.payload.confirmed}</span> 
+          <span className="recovered">Recovered:{payload.payload.recovered}</span>
+          <span className="active">Active: {payload.payload.active}</span>
+          <span className="death">Death: {payload.payload.deceased}</span>          
+          </>
+        }
+        </div>
     }
     const caseType = ()=>{
       switch(filterCaseType) {
@@ -82,12 +101,36 @@ const TinyBarGraph = props =>{
     return (
         <>
            
-                <div className="tiny-bar-chart">
+                <div className={`"tiny-bar-chart ${thememode}`} id="tiny-bar-chart">
                   <BarChart width={chartWidth} height={chartHeight}   data={props.latestData}
                         margin={{top: 15, right: 30, left: 20, bottom: 5}}>
                     <XAxis interval={0} dataKey={props.xDataKey} tick={<CustomizedAxisTick/>}/>
-                    <Tooltip/>
-                    {caseType()}            
+                    <Tooltip content={tooltipContent}/>
+                    {
+                      props.filterCaseType === 'Confirmed' && 
+                      <Bar barSize={25} dataKey= {props.dataKey.confirmed} fill="#e26868" >
+                      <LabelList dataKey= {props.dataKey.confirmed} content={renderCustomizedLabel}/>
+                      </Bar>
+                    }
+                    {
+                      props.filterCaseType === 'Recovered' && 
+                      <Bar barSize={25} dataKey= {props.dataKey.recovered} fill="#78b16b" >
+                        <LabelList dataKey= {props.dataKey.recovered} content={renderCustomizedLabel} />
+                      </Bar>
+                    }
+                    {
+                      filterCaseType === 'Deceased' && 
+                      <Bar barSize={25} dataKey= {props.dataKey.deceased} fill="grey" >
+                        <LabelList dataKey= {props.dataKey.deceased} content={renderCustomizedLabel} />
+                      </Bar>
+                    }
+                    {
+                      props.filterCaseType === 'Active' && 
+                        <Bar barSize={25} dataKey= {props.dataKey.active} fill="#7e78f7" >
+                        <LabelList dataKey= {props.dataKey.active} content={renderCustomizedLabel} />
+                      </Bar>
+                    }
+                            
                   </BarChart>       
                 </div>
             
