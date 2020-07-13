f0bc0e2d6f86854fbe86f987b42235533ca4bfda
skshailendra <singh.shailendra.ind2@gmail.com>
2020-07-13 19:34:57 +0530

    Adding Active cases

--- src/DisplayTotal/DisplayTotal.js
+++ src/DisplayTotal/DisplayTotal.js
@@ -1,147 +1,180 @@
-import React , {useState, useEffect, useContext}from 'react';
-import './DisplayTotal.scss';
-import { faArrowUp} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {FetchDataContext} from '../context/fetch-data';
-import LineChartComponent from '../GraphComponent/LineChartComponent/LineChartComponent';
-import TinyAreaGraph from '../GraphComponent/TinyAreaGraph/TinyAreaGraph';
-import {ThemeContext} from '../context/theme';
-import CountUp from 'react-countup';
+import React, { useState, useEffect, useContext } from "react";
+import "./DisplayTotal.scss";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FetchDataContext } from "../context/fetch-data";
+import LineChartComponent from "../GraphComponent/LineChartComponent/LineChartComponent";
+import TinyAreaGraph from "../GraphComponent/TinyAreaGraph/TinyAreaGraph";
+import { ThemeContext } from "../context/theme";
+import CountUp from "react-countup";
 const monthList = {
-    1:"January",
-    2:"February",
-    3:"March",
-    4:"April",
-    5:"May",
-    6:"June",
-    7:"July",
-    8:"August",
-    9:"September",
-    10:"October",
-    11:"November",
-    12:"December"
+  1: "January",
+  2: "February",
+  3: "March",
+  4: "April",
+  5: "May",
+  6: "June",
+  7: "July",
+  8: "August",
+  9: "September",
+  10: "October",
+  11: "November",
+  12: "December",
 };
-const getCurrentMonth = ()=>{
-    let selectedMonth = new Date().getDate() < 10 ? new Date().getMonth() : new Date().getMonth() + 1;
-    return monthList[selectedMonth];
+const getCurrentMonth = () => {
+  let selectedMonth =
+    new Date().getDate() < 10
+      ? new Date().getMonth()
+      : new Date().getMonth() + 1;
+  return monthList[selectedMonth];
 };
-const DisplayTotal = props =>{
+const DisplayTotal = (props) => {
+  const fetchCovidData = useContext(FetchDataContext);
+  const casesTimeSeries = fetchCovidData.casesTimeSeries;
+  const statewise = fetchCovidData.statewise[0];
+  const [latestData, setLatestData] = useState([]);
+  const { thememode } = useContext(ThemeContext);
+  useEffect(() => {
+    let filterArray = [];
+    if (Array.isArray(casesTimeSeries) && casesTimeSeries.length > 0) {
+      filterArray = casesTimeSeries.filter((item) =>
+        item.date.includes(getCurrentMonth())
+      );
+      filterArray.map((item) => {
+        item.dailyconfirmed = Math.abs(parseInt(item.dailyconfirmed));
+        item.dailydeceased = Math.abs(parseInt(item.dailydeceased));
+        item.dailyrecovered = Math.abs(parseInt(item.dailyrecovered));
+        item.dailyactive =
+          item.dailyconfirmed - (item.dailyrecovered + item.dailydeceased);
+        item.totalconfirmed = parseInt(item.totalconfirmed);
+        item.totaldeceased = parseInt(item.totaldeceased);
+        item.totalrecovered = parseInt(item.totalrecovered);
+        item.totalactive =
+          item.totalconfirmed - (item.totalrecovered + item.totaldeceased);
+        return item;
+      });
+      setLatestData(filterArray);
+    }
+  }, [casesTimeSeries]);
 
-    const fetchCovidData = useContext(FetchDataContext);
-    const casesTimeSeries = fetchCovidData.casesTimeSeries;
-    const statewise = fetchCovidData.statewise[0];
-    const [latestData,setLatestData] = useState([]);
-    const {thememode} = useContext(ThemeContext);
-    useEffect(()=>{
-        let filterArray = [];
-        if(Array.isArray(casesTimeSeries) && casesTimeSeries.length > 0){
-            filterArray = casesTimeSeries.filter( (item)=>item.date.includes(getCurrentMonth() ));
-            filterArray.map((item) => {
-                item.dailyconfirmed = Math.abs(parseInt(item.dailyconfirmed));
-                item.dailydeceased  = Math.abs(parseInt(item.dailydeceased));
-                item.dailyrecovered = Math.abs(parseInt(item.dailyrecovered));
-                item.dailyactive    = item.dailyconfirmed - (item.dailyrecovered + item.dailydeceased);
-                item.totalconfirmed = parseInt(item.totalconfirmed);
-                item.totaldeceased = parseInt(item.totaldeceased);
-                item.totalrecovered = parseInt(item.totalrecovered);
-                item.totalactive =    item.totalconfirmed - (item.totalrecovered + item.totaldeceased);
-                return item;
-            });
-            setLatestData(filterArray);
-        }
-    },[casesTimeSeries]);
-
-    return (
-    <>  
-            <div className={`container ${thememode}`}>
-                <div className={`display-total ${thememode}`}>
-                    {statewise && 
-                        <>
-                        <div className={`display-total__block ${thememode}`}>
-                            <div className={`display-total__text confirmed`}>
-                                {"Confirmed"}
-                            </div>
-                            <div className={`display-total__count-block`}>
-                                <div className="display-total__count">
-                                    <CountUp
-                                    start={200000}
-                                    end={parseInt(statewise.confirmed)}
-                                    duration={2.1}
-                                    separator=","
-                                    />                              
-                                </div>
-                                
-                                <div className="display-total__increase">
-                                    <FontAwesomeIcon icon={faArrowUp}  size="lg" className="display-total__icon"/>
-                                    [+{Math.abs(parseInt(statewise.deltaconfirmed))}]
-                                </div>
-                                
-                            </div>
-                            <TinyAreaGraph latestData= {latestData} dataKey={"dailyconfirmed"} fillcolor={"red"}/>
-                        </div>
-
-                        <div className={`display-total__block ${thememode}`}>
-                            <div className={`display-total__text recovered`}>
-                                {"Recovered"}
-                            </div>
-                            <div className={`display-total__count-block`}>
-                                <div className="display-total__count">
-                                    <CountUp
-                                    start={200000}
-                                    end={parseInt(statewise.recovered)}
-                                    duration={2.1}
-                                    separator=","
-                                    />
-                                </div>
-                                <div className="display-total__increase">
-                                    <FontAwesomeIcon icon={faArrowUp}  size="lg" className="display-total__icon"/>
-                                    [+{Math.abs(parseInt(statewise.deltarecovered))}]
-                                </div>
-                            </div>
-                            <TinyAreaGraph latestData= {latestData} dataKey={"dailyrecovered"} fillcolor={"green"}/>
-                        </div>
-                        {/* <div className={`display-total__block`}>
-                            <div className={`display-total__text`}>
-                                {"Active"}
-                            </div>
-                            <div className={`display-total__count-block`}>
-                                <div className="display-total__count">
-                                    {statewise.active}
-                                </div>
-                            </div>
-                            <TinyAreaGraph latestData= {latestData} dataKey={"dailyactive"} fillcolor={"green"}/>
-                        </div>     */}
-
-                        <div className={`display-total__block ${thememode}`}>
-                            <div className={`display-total__text deaths`}>
-                                {"Deaths"}
-                            </div>
-                            <div className={`display-total__count-block`}>
-                                <div className="display-total__count">
-                                    <CountUp
-                                    start={0}
-                                    end={parseInt(statewise.deaths)}
-                                    duration={2.1}
-                                    separator=","
-                                    />
-                                </div>                                
-                                <div className="display-total__increase">
-                                    <FontAwesomeIcon icon={faArrowUp}  size="lg" className="display-total__icon"/>
-                                    [+{Math.abs(parseInt(statewise.deltadeaths))}]
-                                </div>
-                            </div>
-                            <TinyAreaGraph latestData= {latestData} dataKey={"dailydeceased"} fillcolor={"grey"}/>
-                        </div>
-                        </>   
-                    }
-                    
+  return (
+    <>
+      <div className={`container ${thememode}`}>
+        <div className={`display-total ${thememode}`}>
+          {statewise && (
+            <>
+              <div className={`display-total__block ${thememode}`}>
+                <div className={`display-total__text confirmed`}>
+                  {"Confirmed"}
                 </div>
-                <div className="display-line-chart-container">
-                    <LineChartComponent/>
+                <div className={`display-total__count-block`}>
+                  <div className="display-total__count">
+                    <CountUp
+                      start={200000}
+                      end={parseInt(statewise.confirmed)}
+                      duration={2.1}
+                      separator=","
+                    />
+                  </div>
+
+                  <div className="display-total__increase">
+                    <FontAwesomeIcon
+                      icon={faArrowUp}
+                      size="lg"
+                      className="display-total__icon"
+                    />
+                    [+{Math.abs(parseInt(statewise.deltaconfirmed))}]
+                  </div>
                 </div>
-            </div>   
-        </>
-    );
+                <TinyAreaGraph
+                  latestData={latestData}
+                  dataKey={"dailyconfirmed"}
+                  fillcolor={"red"}
+                />
+              </div>
+
+              <div className={`display-total__block ${thememode}`}>
+                <div className={`display-total__text recovered`}>
+                  {"Recovered"}
+                </div>
+                <div className={`display-total__count-block`}>
+                  <div className="display-total__count">
+                    <CountUp
+                      start={200000}
+                      end={parseInt(statewise.recovered)}
+                      duration={2.1}
+                      separator=","
+                    />
+                  </div>
+                  <div className="display-total__increase">
+                    <FontAwesomeIcon
+                      icon={faArrowUp}
+                      size="lg"
+                      className="display-total__icon"
+                    />
+                    [+{Math.abs(parseInt(statewise.deltarecovered))}]
+                  </div>
+                </div>
+                <TinyAreaGraph
+                  latestData={latestData}
+                  dataKey={"dailyrecovered"}
+                  fillcolor={"green"}
+                />
+              </div>
+              <div className={`display-total__block  ${thememode}`}>
+                <div className={`display-total__text active`}>{"Active"}</div>
+                <div className={`display-total__count-block`}>
+                  <div className="display-total__count">
+                    <CountUp
+                      start={200000}
+                      end={parseInt(statewise.active)}
+                      duration={2.1}
+                      separator=","
+                    />
+                  </div>
+                </div>
+                <TinyAreaGraph
+                  latestData={latestData}
+                  dataKey={"dailyactive"}
+                  fillcolor={"blue"}
+                />
+              </div>
+
+              <div className={`display-total__block ${thememode}`}>
+                <div className={`display-total__text deaths`}>{"Deaths"}</div>
+                <div className={`display-total__count-block`}>
+                  <div className="display-total__count">
+                    <CountUp
+                      start={0}
+                      end={parseInt(statewise.deaths)}
+                      duration={2.1}
+                      separator=","
+                    />
+                  </div>
+                  <div className="display-total__increase">
+                    <FontAwesomeIcon
+                      icon={faArrowUp}
+                      size="lg"
+                      className="display-total__icon"
+                    />
+                    [+{Math.abs(parseInt(statewise.deltadeaths))}]
+                  </div>
+                </div>
+                <TinyAreaGraph
+                  latestData={latestData}
+                  dataKey={"dailydeceased"}
+                  fillcolor={"grey"}
+                />
+              </div>
+            </>
+          )}
+        </div>
+        <div className="display-line-chart-container">
+          <LineChartComponent />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default DisplayTotal;
--- src/GraphComponent/LineGraph/LineGraph.js
+++ src/GraphComponent/LineGraph/LineGraph.js
@@ -1,47 +1,82 @@
-
-import React, { useEffect, useState,useContext} from 'react';
-import './LineGraph.scss';
+import React, { useEffect, useState, useContext } from "react";
+import "./LineGraph.scss";
 import {
-  XAxis, YAxis, CartesianGrid, Tooltip,LineChart, Line,Legend
-  } from 'recharts';
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  LineChart,
+  Line,
+  Legend,
+} from "recharts";
 
-import useDeviceAgent from '../../hooks/device-agent';
-import {ThemeContext} from '../../context/theme';
-const LineGraph = props =>{
-  const {device} = useDeviceAgent();
+import useDeviceAgent from "../../hooks/device-agent";
+import { ThemeContext } from "../../context/theme";
+const LineGraph = (props) => {
+  const { device } = useDeviceAgent();
   const [chartWidth, setChartWidth] = useState(800);
   const [chartHeight, setChartHeight] = useState(400);
-  const {thememode} = useContext(ThemeContext);
-  useEffect(()=>{
-    if(device && device.isExtraLargeDevice){
-      setChartWidth(700);setChartHeight(400);
+  const { thememode } = useContext(ThemeContext);
+  useEffect(() => {
+    if (device && device.isExtraLargeDevice) {
+      setChartWidth(700);
+      setChartHeight(500);
     }
-    if(device && device.isLargeDevice){
-      setChartWidth(700);setChartHeight(400);
+    if (device && device.isLargeDevice) {
+      setChartWidth(700);
+      setChartHeight(500);
     }
-    if(device && device.isMediumDevice){
-      setChartWidth(700);setChartHeight(400);
+    if (device && device.isMediumDevice) {
+      setChartWidth(700);
+      setChartHeight(400);
     }
-    if(device && device.isSmallDevice){
-      setChartWidth(300);setChartHeight(350);
+    if (device && device.isSmallDevice) {
+      setChartWidth(300);
+      setChartHeight(350);
     }
-  },[device]);
-    return (
-        <>
-          <div className={`line-chart ${thememode}`}>
-            <LineChart width={chartWidth} height={chartHeight} data={props.latestData}
-                 scale="auto" >
-            <XAxis dataKey="date"/>
-            <YAxis  domain={['auto',  dataMax => {return (dataMax)}]}/>
-            <CartesianGrid vertical={false}  strokeDasharray="3 3"/>
-            <Tooltip/>
-            <Legend />
-            <Line type="monotone" dataKey="dailyconfirmed" stroke="red" activeDot={{r: 8}}/>
-            <Line type="monotone" dataKey="dailyrecovered" stroke="#206111" activeDot={{r: 8}}/>
-            <Line type="monotone" dataKey="dailydeceased" stroke="#525050" activeDot={{r: 8}}/>
-            </LineChart>        
-          </div>
-        </>
-    );
-}
+  }, [device]);
+  return (
+    <>
+      <div className={`line-chart ${thememode}`}>
+        <LineChart
+          width={chartWidth}
+          height={chartHeight}
+          data={props.latestData}
+          scale="auto"
+        >
+          <XAxis dataKey="date" />
+          <YAxis
+            domain={[
+              "auto",
+              (dataMax) => {
+                return dataMax;
+              },
+            ]}
+          />
+          <CartesianGrid vertical={false} strokeDasharray="3 3" />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="dailyconfirmed"
+            stroke="red"
+            activeDot={{ r: 8 }}
+          />
+          <Line
+            type="monotone"
+            dataKey="dailyrecovered"
+            stroke="#206111"
+            activeDot={{ r: 8 }}
+          />
+          <Line
+            type="monotone"
+            dataKey="dailydeceased"
+            stroke="#525050"
+            activeDot={{ r: 8 }}
+          />
+        </LineChart>
+      </div>
+    </>
+  );
+};
 export default LineGraph;
